8e63adee4671a9477efc5df9f7fdf64145b46983
Emmanuel Alozie <emmanuelalozie@Emmanuels-MacBook-Pro.local>
2024-06-12 15:39:55 +0100

    feat: go back to the homepage by clicking the logo

--- src/components/container/block/header.tsx
+++ src/components/container/block/header.tsx
@@ -1,18 +1,16 @@
 import ConnectWallet from "@/components/cta/ConnectWalletButton";
 import { Button } from "../../ui/button";
 import wallet from "@/assets/template/wallet.svg";
-import { Link } from "react-router-dom";
 import { useWeb3Context } from "@/context/Web3Context";
 import { useDisconnect } from "@web3modal/ethers/react";
 
 export default function Header() {
   const web3Context = useWeb3Context();
-  const { disconnect } = useDisconnect()
-
+  const { disconnect } = useDisconnect();
 
   return (
     <header className="flex items-center justify-between py-4 md:py-2 px-5 md:px-10 lg:px-14 xl:px-16 2xl:px-20">
-      <Link to={"/"}>
+      <a href="https://nft.nurika.health/">
         <div className="flex items-center space-x-2">
           <img
             src="/logo-text.svg"
@@ -20,7 +18,7 @@ export default function Header() {
             className="w-24 sm:w-28 md:w-32"
           />
         </div>
-      </Link>
+      </a>
 
       {!web3Context.isConnected && (
         <ConnectWallet>
@@ -30,7 +28,9 @@ export default function Header() {
           </Button>
         </ConnectWallet>
       )}
-      {web3Context.isConnected && <Button onClick={() => disconnect()}>Disconnect</Button>}
+      {web3Context.isConnected && (
+        <Button onClick={() => disconnect()}>Disconnect</Button>
+      )}
     </header>
   );
 }
