18d711e9a1e688c9f06a1fcfc6da91e5336b4a3f
ConfidenceYobo <confiyobo@gmail.com>
2024-06-24 12:39:44 +0100

    add polygon mainnet address

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -3,7 +3,7 @@ import { ChainId } from "@/types";
 const contractAddresses = {
   nft: {
     [ChainId.TESTNET]: "0x70ed82ea4f4CE5Dc5C8ab601eAae6FFa8aDac244",
-    [ChainId.MAINNET]: "",
+    [ChainId.MAINNET]: "0x9833C9F0FcCd8891CB86670a37Bd819cEF47aa40",
   },
 };
 
